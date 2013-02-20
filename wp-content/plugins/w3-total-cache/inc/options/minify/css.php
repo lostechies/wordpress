098546267aa59eba95f324e2969f88535ed96b5e
jmeridth <jmeridth@gmail.com>
2013-02-20 14:25:06 -0500

    remove w3total cache due to issues of renaming post links on homepage

--- wp-content/plugins/w3-total-cache/inc/options/minify/css.php
+++
@@ -1,3 +0,0 @@
-<?php if (!defined('W3TC')) die(); ?>
-<?php $this->checkbox('minify.css.strip.comments', false, 'css_') ?> Preserved comment removal (not applied when combine only is active)</label><br />
-<?php $this->checkbox('minify.css.strip.crlf', false, 'css_') ?> Line break removal (not applied when combine only is active)</label><br />
--- wp-content/plugins/w3-total-cache/inc/options/minify/html.php
+++
@@ -1,2 +0,0 @@
-<?php if (!defined('W3TC')) die(); ?>
-<?php $this->checkbox('minify.html.strip.crlf', false, 'html_') ?> Line break removal</label><br />
